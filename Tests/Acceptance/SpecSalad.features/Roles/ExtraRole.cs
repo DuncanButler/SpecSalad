ba1867aaf14ad16f80cc155acdc12a2e0a69bf0e
Mike Hanson <mike@mikehanson.com>
2014-08-15 22:09:55 +0100

    Added extra roles to support testing of an syntax

---
+++ Tests/Acceptance/SpecSalad.features/Roles/ExtraRole.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SpecSalad.features.Roles
+{
+    public class ExtraRole : ApplicationRole
+    {
+        public ExtraRole()
+        {
+            TheAnswer = 0;
+        }
+
+        int TheAnswer 
+        { 
+            get
+            {
+                object result = this.Retrieve("Total");
+
+                if (result == null)
+                    return 0;
+
+                return (int) result; 
+            }
+            set
+            {
+                this.StoreValue("Total",value);
+            }
+        }
+       
+        public bool Add(int theValue)
+        {
+            TheAnswer += theValue;
+
+            return true;
+        }
+
+        public int GetTheAnswer()
+         {
+             return TheAnswer;
+         }
+
+        public ICollection GetTheAnswers()
+        {
+            var list = new List<string>();
+            list.Add(TheAnswer.ToString());
+
+            return list;
+        }
+
+        public void ShouldContainOne()
+        {
+            Assert.AreEqual(1,TheAnswer);
+        }
+
+        public object GetStoredTaskAnswer()
+        {
+            return  this.Retrieve("FromTask");
+        }
+
+		public void CheckTheAnswerIs(string answer)
+		{
+			Assert.That(answer, Is.EqualTo("1"));
+		}
+    }
+}
---
+++ Tests/Acceptance/SpecSalad.features/Roles/ExtraSecondaryRole.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SpecSalad.features.Roles
+{
+    public class ExtraSecondaryRole : ApplicationRole
+    {
+        public ExtraSecondaryRole()
+        {
+            this.StoreValue("Total",0);
+        }
+
+        public bool Add(int theValue)
+        {
+            int total = (int) this.Retrieve("Total");
+
+            total += 2;
+
+            this.StoreValue("Total",total);
+
+            return true;
+        }
+
+         public void SubtractOne()
+         {
+             int total = (int)this.Retrieve("Total");
+
+             total -= 1;
+
+             this.StoreValue("Total",total);
+         }
+
+        public int GetTheAnswer()
+        {
+            return (int) this.Retrieve("Total");
+        }
+
+        public void ShouldContainOne()
+        {
+            Assert.That(this.Retrieve("Total"),Is.EqualTo(1));
+        }
+
+        public ICollection GetTheAnswers()
+        {
+            var list = new List<string>();
+            list.Add(this.Retrieve("Total").ToString());
+
+            return list;
+        }
+
+    }
+}
